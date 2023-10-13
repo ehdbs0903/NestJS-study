15eb066d06e5309ee4f421b828541572404085b7
username <ehdbs8747@naver.com>
2023-10-13 16:52:21 +0900

    feat: auth setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig), BoardsModule, AuthModule],
 })
 export class AppModule {}
